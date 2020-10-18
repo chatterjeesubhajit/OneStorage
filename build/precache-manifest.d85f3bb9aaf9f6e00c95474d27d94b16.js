d17487cc4d50606366b3cdc41acc4de79ec343ee
Subhajit Chatterjee <chatterje.subhajit@gmail.com>
2020-10-18 01:30:46 -0500

    initial commit

---
+++ build/precache-manifest.d85f3bb9aaf9f6e00c95474d27d94b16.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9906deac467ec136968ffb4f899eb4a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8061b35058737949fe00",
+    "url": "/static/css/2.3f04b7d5.chunk.css"
+  },
+  {
+    "revision": "97e85f373edad1bfb339",
+    "url": "/static/css/main.6f05ade8.chunk.css"
+  },
+  {
+    "revision": "8061b35058737949fe00",
+    "url": "/static/js/2.a96f59bf.chunk.js"
+  },
+  {
+    "revision": "323df8395f8ea9cf3ba2c443ad39e295",
+    "url": "/static/js/2.a96f59bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97e85f373edad1bfb339",
+    "url": "/static/js/main.2197bc47.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "1ca345074eac11876d6dad5709ff6e8a",
+    "url": "/static/media/oneStorage.1ca34507.png"
+  }
+]);
